485ec44012b4c35c0cb03b588ceafb8fc1d4ec91
Brighton Ulery <brighton.ulery@yahoo.com>
2016-08-15 20:12:07 -0500

    Began tengu fan implementation

---
+++ src/main/java/io/github/herpahermaderp/japmythc/item/ItemTenguFan.java
@@ -0,0 +1,14 @@
+package io.github.herpahermaderp.japmythc.item;
+
+import io.github.herpahermaderp.japmythc.lib.Reference;
+import net.minecraft.item.ItemSword;
+
+public class ItemTenguFan extends ItemSword {
+	
+	public ItemTenguFan(String unlocalizedName, ToolMaterial material) {
+		
+		super(material);
+		this.setUnlocalizedName(unlocalizedName);
+		this.setTextureName(Reference.ID + ":" + unlocalizedName);
+	}
+}
--- src/main/java/io/github/herpahermaderp/japmythc/item/ModItems.java
+++ src/main/java/io/github/herpahermaderp/japmythc/item/ModItems.java
@@ -2,13 +2,22 @@ package io.github.herpahermaderp.japmythc.item;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems {
 
 	public static Item charm;
+	public static ItemSword tenguFan;
+	
+	public static ToolMaterial FANBLOOM = EnumHelper.addToolMaterial("Fan_bloom", 0, 131, 0, 0, 0);
+	public static ToolMaterial FANTENGU = EnumHelper.addToolMaterial("fan_tengu", 0, 131, 0, 3.0F, 10);
+	public static ToolMaterial FANWIND = EnumHelper.addToolMaterial("fan_wind", 0, 131, 0, 0, 0);
 	
 	public static final void init() {
 		
 		GameRegistry.registerItem(charm = new ItemCharm("charm"), "charm");
+		GameRegistry.registerItem(tenguFan = new ItemTenguFan("tengu_fan", FANTENGU), "tengu_fan");
 	}
 }
