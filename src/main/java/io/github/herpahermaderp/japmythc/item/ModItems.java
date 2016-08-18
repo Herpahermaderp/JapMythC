7f623ba87db8b8f0055c7864466952af243ec107
Brighton Ulery <brighton.ulery@yahoo.com>
2016-08-18 18:53:10 -0500

    *ATTEMPTED* implementation of the katana (failed)

--- src/main/java/io/github/herpahermaderp/japmythc/item/ModItems.java
+++ src/main/java/io/github/herpahermaderp/japmythc/item/ModItems.java
@@ -10,15 +10,18 @@ public class ModItems {
 
 	public static Item bloomFan;
 	public static Item charm;
+	public static ItemSword katana;
 	public static Item windFan;
 	public static ItemSword tenguFan;
 	
+	public static ToolMaterial KATANA = EnumHelper.addToolMaterial("katana", 0, 250, 0, 3.0F, 10);
 	public static ToolMaterial FANTENGU = EnumHelper.addToolMaterial("fan_tengu", 0, 131, 0, 3.0F, 10);
 	
 	public static final void init() {
 		
 		GameRegistry.registerItem(bloomFan = new ItemBloomFan("blooming_fan"), "blooming_fan");
 		GameRegistry.registerItem(charm = new ItemCharm("charm"), "charm");
+		GameRegistry.registerItem(katana = new ItemKatana("katana", KATANA), "katana");
 		GameRegistry.registerItem(tenguFan = new ItemTenguFan("tengu_fan", FANTENGU), "tengu_fan");
 		GameRegistry.registerItem(windFan = new ItemWindFan("wind_fan"), "wind_fan");
 	}
